2b6dc1c8dd841872709a875e871f4279afa4b200
xattacker <xattacker.tao@gmail.com>
2020-08-26 13:49:52 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -21,8 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "PolygonAPI",
- 	    path: "PolygonAPI/Sources",
-            dependencies: []),
+ 	    path: "PolygonAPI/Sources"),
     ],
     swiftLanguageVersions: [.v5]
 )
