381418c37293d567db04ed7c6154b1cae7b2df39
xattacker <xattacker.tao@gmail.com>
2020-08-26 14:08:25 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Polygon_iOS_API",
+    name: "PolygonAPI",
     platforms: [.iOS(.v9)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
